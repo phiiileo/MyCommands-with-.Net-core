ffa4073e3f96598081659986f6346a9b74d21d51
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-08-28 00:08:31 +0100

    setup mock up endpoints

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+           var commands = new List<Command>
+           {
+               new Command{Id=0, HowTo="Boil an egg", Line="Boil egg", Platform="Pot and Pan"},
+               new Command{Id=1, HowTo="Kill an egg", Line="Kill egg", Platform="Pot and Pan"},
+               new Command{Id=2, HowTo="Spray an egg", Line="Spray egg", Platform="Pot and Pan"},
+               new Command{Id=3, HowTo="Spill an egg", Line="Spill egg", Platform="Pot and Pan"},
+           };
+
+           return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+           return new Command{Id=0, HowTo="Boil an egg", Line="Boil egg", Platform="Pot and Pan"};
+        }
+    }
+}
